6deef333c81e93746b67e373fe983738bd455b9e
Асафьев Александр <asafjew2015@yandex.ru>
2020-04-30 00:59:21 +0300

    Добавьте файлы проекта.

---
+++ Laba2/Laba2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void F1(double*& mas, int& n)
+{
+	int m = (n * 2) - 1;
+	double* mas2 = new double[m];
+	int j = 0;
+	for (int i = 0; i < n; i++)
+	{
+		mas2[j] = mas[i];
+		mas2[j + 1] = 0;
+		j = j + 2;
+	}
+	n = m;
+	mas = mas2;
+
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Ввидиьте длину массива" << " ";
+	cin >> n;
+	cout << "Ввидите массив" << endl;
+	double* mas = new double[n];
+	for (int i = 0; i < n; i++) { cin >> mas[i]; }
+	F1(mas, n);
+	for (int i = 0; i < n; i++) { cout << mas[i] << " "; }
+	system("pause");
+	return 0;
+}
